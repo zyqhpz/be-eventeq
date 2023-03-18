73d8317b92741468736e3fccc7d43534b0670a10
Haziq Hapiz <zyqq.dev@gmail.com>
2023-03-19 01:02:26 +0800

    add user login function to authenticate from MongoDB

--- Controllers/user_controller.go
+++ Controllers/user_controller.go
@@ -1,29 +1,84 @@
 package api
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
+	db "github.com/zyqhpz/be-eventeq/Database"
 	model "github.com/zyqhpz/be-eventeq/Models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-var users = []model.User{
-	{ID: 1, Username: "admin", Password: "admin", Role: 0, Email: "admin@gmail.com"},
-	{ID: 2, Username: "user", Password: "user", Role: 1, Email: "user@gmail.com"},
+func ConnectDBUsers() (*mongo.Collection, error) {
+	client, err  := db.ConnectDB()
+	ctx := context.Background()
+	defer client.Disconnect(ctx)
+
+	// Get a handle to the "users" collection
+	collection := client.Database("eventeq").Collection("users")
+
+	return collection, err
 }
 
 func LoginUser(c *fiber.Ctx) error {
 	username := c.FormValue("username")
 	password := c.FormValue("password")
 
-	if username == "admin" && password == "admin" {
+	client, err  := db.ConnectDB()
+	ctx := context.Background()
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("eventeq").Collection("users")
+
+	// Define a filter to find the user with the given username and password
+    filter := bson.M{"username": username, "password": password}
+
+    // Count the number of documents that match the filter
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+    count, err := collection.CountDocuments(ctx, filter)
+    if err != nil {
+        return c.JSON(fiber.Map{"message": "Login Failed " + username})
+    }
+
+    // Return true if a user with the given username and password was found, false otherwise
+    if (count > 0) {
 		fmt.Println("Username: ", username, "Login Success")
 		return c.JSON(fiber.Map{"message": "Login Success"})
+	} else {
+		return c.JSON(fiber.Map{"message": "Login Failed " + username})
 	}
-	return c.JSON(fiber.Map{"message": "Login Failed " + username})
 }
 
 func GetUsers(c *fiber.Ctx) error {
+
+	ctx := context.Background()
+
+	// collection, _ := ConnectDBUsers()
+
+	client, err  := db.ConnectDB()
+	collection := client.Database("eventeq").Collection("users")
+
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	// Iterate through the documents and print them
+	var users []model.User
+	for cursor.Next(ctx) {
+		var user model.User
+		if err := cursor.Decode(&user); err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
 	return c.JSON(users)
 }
