56cdd9deeabb325833bbf6a7743a04d1620078c3
Haziq Hapiz <zyqq.dev@gmail.com>
2023-03-19 01:02:06 +0800

    add new variable in User model

--- Models/User.go
+++ Models/User.go
@@ -2,6 +2,7 @@ package model
 
 type User struct {
 	ID       uint   `json:"id"`
+	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Role     int    `json:"role"`
