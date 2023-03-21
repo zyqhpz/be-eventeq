dd8adc237ffa58814de7dc22741639517c0f244a
Haziq Hapiz <zyqq.dev@gmail.com>
2023-03-21 23:38:29 +0800

    add timestamp to User model

--- Models/User.go
+++ Models/User.go
@@ -1,12 +1,20 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	FirstName string             `bson:"first_name"`
-	LastName  string             `bson:"last_name"`
-	Username  string             `bson:"username"`
-	Password  string             `bson:"password"`
-	Email     string             `bson:"email"`
+	ID        primitive.ObjectID 	`bson:"_id,omitempty"`
+	FirstName string             	`bson:"first_name"`
+	LastName  string             	`bson:"last_name"`
+	Username  string             	`bson:"username"`
+	Password  string             	`bson:"password"`
+	Email     string             	`bson:"email"`
+
+	// Timestamp
+	CreatedAt time.Time 			`bson:"created_at"`
+	UpdatedAt time.Time 			`bson:"updated_at"`
 }
