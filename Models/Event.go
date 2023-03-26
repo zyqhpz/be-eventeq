417a86fa0f78ac169c16882da6b0848e59e19641
Haziq Hapiz <zyqq.dev@gmail.com>
2023-03-26 23:59:31 +0800

    update struct

--- Models/Event.go
+++ Models/Event.go
@@ -2,14 +2,18 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Event struct {
-	ID          uint   		`bson:"id"`
-	Name        string 		`bson:"name"`
-	Description string 		`bson:"description"`
-	State       string 		`bson:"state"`
-	DateStart   time.Time 	`bson:"date_start"`
-	DateEnd     time.Time 	`bson:"date_end"`
-	OrganizedBy int    		`bson:"organized_by"`
+	ID        primitive.ObjectID 	`bson:"_id,omitempty"`
+	Name        string 				`bson:"name"`
+	Description string 				`bson:"description"`
+	Location	Location			`bson:"location"`
+	DateStart   time.Time 			`bson:"date_start"`
+	DateEnd     time.Time 			`bson:"date_end"`
+	OrganizedBy primitive.ObjectID	`bson:"organized_by"`
+	CreatedAt 	time.Time 			`bson:"created_at"`
+	UpdatedAt 	time.Time 			`bson:"updated_at"`
 }
--- Models/Item.go
+++ Models/Item.go
@@ -2,26 +2,33 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Item struct {
-	ID          uint   `bson:"id"`
-	Name        string `bson:"name"`
-	Description string `bson:"description"`
-	Type        int    `bson:"type"`
-	Price       int    `bson:"price"`
-	Stock       int    `bson:"stock"`
+	ID        	primitive.ObjectID 	`bson:"_id,omitempty"`
+	Name        string 				`bson:"name"`
+	Description string 				`bson:"description"`
+	Category    int32				`bson:"type"`
+	Price       float32    			`bson:"price"`
+	Stock       int32    			`bson:"stock"`
 
 	// Relationship OwnedBy to User id
-	OwnedBy 	int `bson:"owned_by"`
+	OwnedBy 	primitive.ObjectID	`bson:"owned_by"`
 
 	// Timestamp
-	CreatedAt 	time.Time `bson:"created_at"`
-	UpdatedAt 	time.Time `bson:"updated_at"`
+	CreatedAt 	time.Time 			`bson:"created_at"`
+	UpdatedAt 	time.Time 			`bson:"updated_at"`
 
 	// Image
-	Image 		string `bson:"image"`
+	Image 		[]string 			`bson:"image"`
 
-	// Status
-	Status 		int `bson:"status"`
+	/* Status
+		0 = Disabled
+		1 = Available
+		2 = Rented
+		3 = Reserved
+	*/
+	Status 		int32 				`bson:"status"`
 }
