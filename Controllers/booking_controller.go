721081bee38ed8b1d6dc1a1513346da107f9ccf0
Haziq Hapiz <zyqq.dev@gmail.com>
2023-08-29 23:42:05 +0800

    clean SSE

--- Controllers/booking_controller.go
+++ Controllers/booking_controller.go
@@ -1,13 +1,15 @@
 package api
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
 	"sort"
 	"time"
-	"net/http"
 
 	db "github.com/zyqhpz/be-eventeq/Database"
 	model "github.com/zyqhpz/be-eventeq/Models"
@@ -1164,3 +1166,54 @@ func AutoUpdateBookingStatus(c *fiber.Ctx) error {
 	}
 }
 
+type (
+	Client struct {
+		name   string
+		events chan *DashBoard
+	}
+	DashBoard struct {
+		User uint
+	}
+)
+
+// SSE
+func updateDashboard(client *Client) {
+	for {
+		db := &DashBoard{
+			User:         uint(rand.Uint32()),
+		}
+		client.events <- db
+	}
+}
+
+func Handler(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(f)
+}
+
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Client: %v", r.RemoteAddr)
+	client := &Client{name: r.RemoteAddr, events: make(chan *DashBoard, 10)}
+	go updateDashboard(client)
+
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case ev := <-client.events:
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.Encode(ev)
+		fmt.Fprintf(w, "data: %v\n\n", buf.String())
+		fmt.Printf("data: %v\n", buf.String())
+	case <-timeout:
+		fmt.Fprintf(w, ": nothing to sent\n\n")
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	controller "github.com/zyqhpz/be-eventeq/Controllers"
 	"go.mongodb.org/mongo-driver/mongo"
 
-	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-
-		"net/http"
-	"math/rand"
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"time"
 )
 
 var client *mongo.Client
@@ -85,64 +78,11 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/ws", websocket.New(controller.WebSocketChat))
 
 	// SSE route
-	// app.Get("/sse", controller.AutoUpdateBookingStatus)
-
-	app.Get("/sse/validation", adaptor.HTTPHandler(handler(dashbaordHandler)))
+	app.Get("/sse", adaptor.HTTPHandler(controller.Handler(controller.DashboardHandler)))
 
 	app.Post("/user/test/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"message": "Login Success"})
 	})
-}
-
-type (
-	Client struct {
-		name   string
-		events chan *DashBoard
-	}
-	DashBoard struct {
-		User uint
-	}
-)
-
-func dashbaordHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Client: %v", r.RemoteAddr)
-	client := &Client{name: r.RemoteAddr, events: make(chan *DashBoard, 10)}
-	go updateDashboard(client)
-
-	// w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	timeout := time.After(1 * time.Second)
-	select {
-	case ev := <-client.events:
-		var buf bytes.Buffer
-		enc := json.NewEncoder(&buf)
-		enc.Encode(ev)
-		fmt.Fprintf(w, "data: %v\n\n", buf.String())
-		fmt.Printf("data: %v\n", buf.String())
-	case <-timeout:
-		fmt.Fprintf(w, ": nothing to sent\n\n")
-	}
-
-	if f, ok := w.(http.Flusher); ok {
-		f.Flush()
-	}
-}
-
-func updateDashboard(client *Client) {
-	for {
-		db := &DashBoard{
-			User:         uint(rand.Uint32()),
-		}
-		client.events <- db
-	}
-}
-
-func handler(f http.HandlerFunc) http.Handler {
-	return http.HandlerFunc(f)
 }
 
 func main() {
@@ -153,7 +93,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "http://localhost:5173, https://fe-eventeq.vercel.app",
 		AllowMethods: "GET, POST, PUT, DELETE",
 		AllowHeaders: "Origin, Content-Type, Accept, Cache-Control, Connection",
 	}))
